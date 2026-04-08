385d741a6715ada5d903ed5cb155cb028764a94c
pablo savio <psm.lobato@gmail.com>
2026-04-08 14:24:13 -0400

    let arrays
    let numeros/let nomes

---
+++ arrays.js
@@ -0,0 +1,20 @@
+ let numeros = [10, 20, 30, 40];
+console.log(numeros[0]); // 10
+console.log(numeros[3]); // 40 
+
+let nomes = ["Ana", "Bruno", "Carla"];
+console.log(nomes[1]); // Bruno
+console.log(nomes[2]); // Carla
+ 
+
+
+/* for (let contador = 1; contador <=100; contador++) {
+    console.log(contador);
+}     */
+
+   /* 
+const numero = 5;
+
+for (let contador = 1; contador <= 10; contador++) {
+    console.log(`${numero} x ${contador} = ${numero * contador}`);
+}    */
