fb191e26feb4d202941366549165eb8912743dc0
pablo savio <psm.lobato@gmail.com>
2026-04-08 14:36:21 -0400

    for arrays
    contador/mutiplicação

--- arrays.js
+++ arrays.js
@@ -13,8 +13,15 @@ console.log(nomes[2]); // Carla
 }     */
 
    /* 
-const numero = 5;
+/* /*  let numeros = [10, 20, 30, 40];
+//nsole.log(numeros[0]); // 10
+//nsole.log(numeros[3]); // 40  */
 
-for (let contador = 1; contador <= 10; contador++) {
-    console.log(`${numero} x ${contador} = ${numero * contador}`);
-}    */
+/* let nomes = ["Ana", "Bruno", "Carla"];
+//nsole.log(nomes[1]); // Bruno
+//nsole.log(nomes[2]); // Carla */ 
+for (let contador = 1; contador <=10; contador++) {console.log(contador);
+}  
+const numero = 5;
+for (let contador = 1; contador <= 10; contador++) {console.log(`${numero} x ${contador} = ${numero * contador}`);
+}  
